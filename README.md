8cd9351a2bcbd98ce697817608884916f9e592b8
L. Jiang <l.jiang.1024@gmail.com>
2024-03-28 14:20:05 +0800

    docs: 软件工程师文档写作最佳实践

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ title: 软件工程师文档写作最佳实践
 
 对于软件工程师来说，文档写作实团队沟通和合作的必备技能，高质量的文档是实现有效沟通的简单方式。
 
-# 基于Markdown的文档写作
+本文开源地址： [https://github.com/ismdeep/best-practice-in-se-doc-writing](https://github.com/ismdeep/best-practice-in-se-doc-writing)
+
+# Markdown
 
 Markdown 是一种轻量的标记语言，程序员尤为喜爱 Markdown 文档，因其有着简洁、直观和高效等特点。而且很多本地化或者在线的编辑器都支持 Markdown 的语法。本文的目的就是通过 Markdown 语法编辑文档，产出各种交付的文档。
 
--- build.sh
+++ build.sh
@@ -12,5 +12,11 @@ source pandoc.env.sh '3.1.9'
 pandoc -f markdown -t mediawiki README.md -o README.wiki
 
 # Build for pdf via xelatex
+# Prerequisite
+#   1. Install pdflatex xelatex
+#
+#      $ sudo apt update
+#      $ sudo apt install -y texlive-full texlive-xetex texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+#
 pandoc -r markdown-auto_identifiers -w latex README.md -o README-snippet.tex
 xelatex README.tex
